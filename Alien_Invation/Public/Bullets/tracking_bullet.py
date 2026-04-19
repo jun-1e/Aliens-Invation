ff953a0dbc1624a6a726634ad2bcd157323e42eb
jun-1e <jianweiw50@gmail.com>
2026-04-19 14:33:12 +0800

    Delete Alien_Invation directory

--- Alien_Invation/Public/Bullets/tracking_bullet.py
+++
@@ -1,8 +0,0 @@
-from bullet import Bullet
-import pygame
-
-class Tracking_Bullet(Bullet):
-    def __init__(self,ai_game,shooter):
-        super().__init__(ai_game,shooter)
-        self.rect = pygame.Rect(0,0,)
-
